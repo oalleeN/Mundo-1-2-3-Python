9ec6e59f10b61d71e305fe7892cc862ea2f5e94f
oalleeN <devaneiosdoalan@gmail.com>
2024-01-15 01:48:45 -0300

    alteração aula 12

--- aula12.py
+++ aula12.py
@@ -1,10 +1,10 @@
 nome = str(input('Digite seu nome: '))
 
-if nome in 'Alan':
+if nome in 'Tobias':
     print(f'Seu nome é muito bonito, {nome}!')
 elif nome in 'Marta' or nome == 'Tulio':
     print(f'Seu nome é diferente.')
-elif nome in 'Rarisson':
+elif nome in 'Lucas':
     print(f'Parabéns, é seu aniversário hoje!')
 else:
     print('Que droga, seu nome é muito feio!')
